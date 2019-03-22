395e5a1e62ed3f38a54de2b1497b2ddb19f3f162
Guohaoxin <1210188858@qq.com>
2019-03-22 18:23:57 +0800

    20190322182300

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dot2pdf
-dot/gv文件批量转换成PDF/PNG文件
+利用git.exe批量将graphviz生成的dot/gv文件批量转换成PDF/PNG文件。
+需要安装：
+1.graphviz
+2.git
+
+使用说明：
+将需要转换的文件与dot2pdf.sh文件放在同一文件夹内，双击即可执行。
+生成与原dot文件同名的文件：
+例：
+example.dot->example.pdf
+
---
+++ dot2pdf.sh
@@ -0,0 +1,9 @@
+#this file is transverse dot file to pdf for Batch processing
+#writen by guohaoxin
+for f in *.dot; 
+do  
+	echo $f; #printf the file name in the gui
+	t=$(basename include/$f .dot); # define the variable 
+	echo $t>>list.txt; #printf the file list to the txt file for check
+    dot -Tpdf $f -o $t.pdf;	 #dot2pdf
+done
